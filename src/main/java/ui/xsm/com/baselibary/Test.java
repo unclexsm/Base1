5bc37bc8a38d0a5b016e8081666c06a47e017a7e
unclexsm <2820843854@qq.com>
2019-01-21 10:31:05 +0800

    Initial commit

---
+++ src/main/java/ui/xsm/com/baselibary/Test.java
@@ -0,0 +1,22 @@
+package ui.xsm.com.baselibary;
+
+import android.util.Log;
+
+/**
+ * @Created by XSM大叔
+ * @CreateTime on 2019/1/21 09:27
+ * @viesion：1.0.0
+ * @Copyright：
+ * @Website：
+ * @E-Mail Address：2820843854@qq.com
+ * @Discription 功能描述：
+ */
+
+
+public class Test  {
+
+
+    public static void test() {
+        Log.e("TAG", "==========================================");
+    }
+}
